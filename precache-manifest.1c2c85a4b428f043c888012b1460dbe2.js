b9b835454d8fc92db6109cb4ab75eb8f7e83f367
leecoders <als-1994@hanmail.net>
2019-08-19 15:54:02 +0900

    Updates

---
+++ precache-manifest.1c2c85a4b428f043c888012b1460dbe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e807697a0669057633a3d6a2915e35",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7720edcc2f08af13445a",
+    "url": "/movie_app/static/css/main.1523d932.chunk.css"
+  },
+  {
+    "revision": "e85bf0c7afe3d8605f32",
+    "url": "/movie_app/static/js/2.fcb80721.chunk.js"
+  },
+  {
+    "revision": "7720edcc2f08af13445a",
+    "url": "/movie_app/static/js/main.c220b087.chunk.js"
+  },
+  {
+    "revision": "f9c5d4d9cea5f935b75a",
+    "url": "/movie_app/static/js/runtime~main.b05fbdd8.js"
+  }
+]);
